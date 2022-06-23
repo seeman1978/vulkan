aa3b36b7b07c29db0361d8da266e70f9e9a53f27
qiang.wang <qiang.wang@megatronix.co>
2022-06-23 10:58:44 +0800

    create a vulkan instance with WSI extensions enabled

--- ImagePresentation/main.cpp
+++ ImagePresentation/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <dlfcn.h>
 #include <vulkan/vulkan.h>
+#include <vector>
+#include <cstring>
 
 void *load_vulkan_library(){
     void *vulkan_library = dlopen("libvulkan.so.1", RTLD_NOW);
@@ -47,7 +49,69 @@ int main() {
             std::cout << "Could not load global level Vulkan function named: vkCreateInstance." << std::endl;
         }
     }
+    // Get available_extensions
+    uint32_t extensions_count{};
+    if (vkEnumerateInstanceExtensionProperties != nullptr) {
+        VkResult result = vkEnumerateInstanceExtensionProperties(nullptr, &extensions_count, nullptr);
+        if( (result != VK_SUCCESS) || (extensions_count == 0)) {
+            std::cout << "Could not get the number of Instance extensions." <<
+                      std::endl;
+            return -1;
+        }
+    }
+    std::vector<VkExtensionProperties> available_extensions(extensions_count);
+    if (vkEnumerateInstanceExtensionProperties != nullptr) {
+        VkResult result = vkEnumerateInstanceExtensionProperties(nullptr, &extensions_count, available_extensions.data());
+        if ((result != VK_SUCCESS) || (extensions_count == 0)) {
+            std::cout << "Could not enumerate Instance extensions." << std::endl;
+            return -1;
+        }
+    }
+
+    // Get desired_extensions
+    std::vector<char const *> desired_extensions{VK_KHR_SURFACE_EXTENSION_NAME};
+    for (auto &extension: desired_extensions) {
+        bool b_found{false};
+        for (auto available : available_extensions) {
+            if (strcmp(available.extensionName, extension) == 0){
+                b_found = true;
+                break;
+            }
+        }
+        if (!b_found){
+            std::cout << "Extension named '" << extension << "' is not supported."
+                      << std::endl;
+            return -1;
+        }
+    }
     // create a vulkan instance with WSI extensions enabled
+    VkApplicationInfo application_info;
+    application_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    application_info.pNext = nullptr;
+    application_info.pApplicationName = "WQ";
+    application_info.applicationVersion = VK_MAKE_VERSION(1, 2, 3);
+    application_info.pEngineName = "First";
+    application_info.engineVersion = VK_MAKE_VERSION(2, 3, 4);
+    application_info.apiVersion = VK_MAKE_VERSION( 1, 0, 0 );
+
+    VkInstanceCreateInfo instance_create_info;
+    instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instance_create_info.pNext = nullptr;
+    instance_create_info.flags = 0;
+    instance_create_info.pApplicationInfo = &application_info;
+    instance_create_info.enabledLayerCount = 0;
+    instance_create_info.ppEnabledLayerNames = nullptr;
+    instance_create_info.enabledExtensionCount = desired_extensions.size();
+    instance_create_info.ppEnabledExtensionNames = desired_extensions.empty() ? nullptr : &desired_extensions[0];
+
+    VkInstance instance;
+    if (vkCreateInstance != nullptr){
+        VkResult result = vkCreateInstance(&instance_create_info, nullptr, &instance);
+        if( (result != VK_SUCCESS) || (instance == VK_NULL_HANDLE) ) {
+            std::cout << "Could not create Vulkan Instance." << std::endl;
+            return -1;
+        }
+    }
 
     return 0;
 }
