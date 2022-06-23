59746e9b0cde63173908e433ffd3002fb892e7f7
qiang.wang <qiang.wang@megatronix.co>
2022-06-23 13:42:36 +0800

    create a presentation surface

--- ImagePresentation/main.cpp
+++ ImagePresentation/main.cpp
@@ -4,6 +4,11 @@
 #include <vector>
 #include <cstring>
 
+struct WindowParameters{
+    xcb_connection_t*             connection;
+    xcb_window_t                  window;
+};
+
 void *load_vulkan_library(){
     void *vulkan_library = dlopen("libvulkan.so.1", RTLD_NOW);
     if (vulkan_library == nullptr){
@@ -31,6 +36,7 @@ int main() {
                 reinterpret_cast<PFN_vkEnumerateInstanceExtensionProperties>(vkGetInstanceProcAddr(nullptr, "vkEnumerateInstanceExtensionProperties"));
         if (vkEnumerateInstanceExtensionProperties == nullptr){
             std::cout << "Could not load global level Vulkan function named: vkEnumerateInstanceExtensionProperties." << std::endl;
+            return -1;
         }
     }
     PFN_vkEnumerateInstanceLayerProperties  vkEnumerateInstanceLayerProperties;
@@ -39,6 +45,7 @@ int main() {
                 reinterpret_cast< PFN_vkEnumerateInstanceLayerProperties >(vkGetInstanceProcAddr(nullptr, "vkEnumerateInstanceLayerProperties"));
         if (vkEnumerateInstanceLayerProperties == nullptr){
             std::cout << "Could not load global level Vulkan function named: vkEnumerateInstanceLayerProperties." << std::endl;
+            return 1;
         }
     }
     PFN_vkCreateInstance vkCreateInstance;
@@ -47,6 +54,7 @@ int main() {
                 reinterpret_cast<PFN_vkCreateInstance>(vkGetInstanceProcAddr(nullptr, "vkCreateInstance"));
         if (vkCreateInstance == nullptr){
             std::cout << "Could not load global level Vulkan function named: vkCreateInstance." << std::endl;
+            return -1;
         }
     }
     // Get available_extensions
@@ -113,5 +121,33 @@ int main() {
         }
     }
 
+    // Load instance level function
+    PFN_vkCreateXcbSurfaceKHR vkCreateXcbSurfaceKHR;
+    vkCreateXcbSurfaceKHR =
+            reinterpret_cast<PFN_vkCreateXcbSurfaceKHR>(vkGetInstanceProcAddr(instance, "vkCreateXcbSurfaceKHR"));
+    if (vkCreateXcbSurfaceKHR == nullptr){
+        std::cout << "Could not load instance level Vulkan function named: vkCreateXcbSurfaceKHR." << std::endl;
+        return 1;
+    }
+
+    // create a presentation surface
+    int nScreenNum = 0;
+    WindowParameters window_parameters;
+    window_parameters.connection = xcb_connect("vulkan", &nScreenNum);
+    window_parameters.window = xcb_generate_id(window_parameters.connection);
+
+    VkXcbSurfaceCreateInfoKHR surface_create_info;
+    surface_create_info.sType = VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR;
+    surface_create_info.pNext = nullptr;
+    surface_create_info.flags = 0;
+    surface_create_info.connection = window_parameters.connection;
+    surface_create_info.window = window_parameters.window;
+
+    VkSurfaceKHR presentation_surface{VK_NULL_HANDLE};
+    VkResult result = vkCreateXcbSurfaceKHR(instance, &surface_create_info, nullptr, &presentation_surface);
+    if( (VK_SUCCESS != result) || (VK_NULL_HANDLE == presentation_surface) ) {
+        std::cout << "Could not create presentation surface." << std::endl;
+        return -1;
+    }
     return 0;
 }
