5938ef61ce82fb9873a07fec33efb768e6c61be4
wq <seeman1978@163.com>
2022-06-29 00:25:08 +0800

    Getting the capabilities of a presentation surface

--- ImagePresentation/main.cpp
+++ ImagePresentation/main.cpp
@@ -5,8 +5,10 @@
 #include <cstring>
 
 struct WindowParameters{
-    xcb_connection_t*             Connection;
-    xcb_window_t                  Window;
+    xcb_connection_t* connection;
+    xcb_screen_t *screen;
+    xcb_window_t window;
+    xcb_intern_atom_reply_t *atom_wm_delete_window;
 };
 
 void *load_vulkan_library(){
@@ -25,6 +27,46 @@ struct QueueInfo {
     uint32_t FamilyIndex;
     std::vector<float> Priorities;
 };
+
+void init_window(struct WindowParameters &info) {
+    uint32_t width{64};
+    uint32_t height{64};
+
+    uint32_t value_mask, value_list[32];
+
+    //info.window = xcb_generate_id(info.connection);
+
+    value_mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
+    value_list[0] = info.screen->black_pixel;
+    value_list[1] = XCB_EVENT_MASK_KEY_RELEASE | XCB_EVENT_MASK_EXPOSURE;
+
+    xcb_create_window(info.connection, XCB_COPY_FROM_PARENT, info.window, info.screen->root, 0, 0, width, height, 0,
+                      XCB_WINDOW_CLASS_INPUT_OUTPUT, info.screen->root_visual, value_mask, value_list);
+
+    /* Magic code that will send notification when window is destroyed */
+    xcb_intern_atom_cookie_t cookie = xcb_intern_atom(info.connection, 1, 12, "WM_PROTOCOLS");
+    xcb_intern_atom_reply_t *reply = xcb_intern_atom_reply(info.connection, cookie, 0);
+
+    xcb_intern_atom_cookie_t cookie2 = xcb_intern_atom(info.connection, 0, 16, "WM_DELETE_WINDOW");
+    info.atom_wm_delete_window = xcb_intern_atom_reply(info.connection, cookie2, 0);
+
+    xcb_change_property(info.connection, XCB_PROP_MODE_REPLACE, info.window, (*reply).atom, 4, 32, 1,
+                        &(*info.atom_wm_delete_window).atom);
+    free(reply);
+
+    xcb_map_window(info.connection, info.window);
+
+    // Force the x/y coordinates to 100,100 results are identical in consecutive
+    // runs
+    const uint32_t coords[] = {100, 100};
+    xcb_configure_window(info.connection, info.window, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y, coords);
+    xcb_flush(info.connection);
+
+    xcb_generic_event_t *e;
+    while ((e = xcb_wait_for_event(info.connection))) {
+        if ((e->response_type & ~0x80) == XCB_EXPOSE) break;
+    }
+}
 
 int main() {
     // Load vulkan library
@@ -138,19 +180,29 @@ int main() {
     // create a presentation surface
     int nScreenNum = 0;
     WindowParameters window_parameters;
-    window_parameters.Connection = xcb_connect(nullptr, &nScreenNum);
-    if (xcb_connection_has_error(window_parameters.Connection)) {
-        printf("Cannot open display\n");
+    window_parameters.connection = xcb_connect(nullptr, &nScreenNum);
+    if (window_parameters.connection == nullptr || xcb_connection_has_error(window_parameters.connection)) {
+        std::cout << "Unable to make an XCB connection\n";
         return -1;
     }
-    window_parameters.Window = xcb_generate_id(window_parameters.Connection);
+    const xcb_setup_t *setup;
+    xcb_screen_iterator_t iter;
+
+    setup = xcb_get_setup(window_parameters.connection);
+    iter = xcb_setup_roots_iterator(setup);
+    while (nScreenNum-- > 0){
+        xcb_screen_next(&iter);
+    }
+    window_parameters.screen = iter.data;
+    window_parameters.window = xcb_generate_id(window_parameters.connection);
+    init_window(window_parameters);
 
     VkXcbSurfaceCreateInfoKHR surface_create_info;
     surface_create_info.sType = VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR;
     surface_create_info.pNext = nullptr;
     surface_create_info.flags = 0;
-    surface_create_info.connection = window_parameters.Connection;
-    surface_create_info.window = window_parameters.Window;
+    surface_create_info.connection = window_parameters.connection;
+    surface_create_info.window = window_parameters.window;
 
     VkSurfaceKHR presentation_surface{VK_NULL_HANDLE};
     VkResult result = vkCreateXcbSurfaceKHR(instance, &surface_create_info, nullptr, &presentation_surface);
